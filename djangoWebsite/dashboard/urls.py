d42f6349971bc86cc9f33d71518c69c61754fe19
AWireman <andrewwireman4@gmail.com>
2020-03-24 21:34:48 -0400

    redirect, embed

--- djangoWebsite/dashboard/urls.py
+++ djangoWebsite/dashboard/urls.py
@@ -3,5 +3,8 @@ from dashboard import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url('', views.dashboard, name='dashboard'),
+    url(r'^$', views.dashboard, name='dashboard'),
+    url(r'^/redirect$', views.redirect, name='redirect'),
+    url(r'^/site$', views.site, name='site'),
+    #url('site', TemplateView.as_view(template_name="http://google.com")),
 ]
--- djangoWebsite/dashboard/views.py
+++ djangoWebsite/dashboard/views.py
@@ -5,9 +5,16 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import views as auth_views
 from django.contrib import auth
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 @login_required
 def dashboard(request):
     return render(request, 'dashboard.html', {})
 
+def redirect(request):
+    return render(request, 'redirect.html', {})
+
+def site(request):
+    return HttpResponseRedirect("http://google.com")
+    
